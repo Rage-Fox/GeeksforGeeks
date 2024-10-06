cf90d14abf09da4559fe1434b30a000b94d0d6a5
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2024-10-06 07:05:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int n,m;
+    void solve(int i,int j,vector<vector<char>>&grid){
+        if(i>=n || i<0 || j<0 || j>=m || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        solve(i-1,j,grid);
+        solve(i,j-1,grid);
+        solve(i+1,j,grid);
+        solve(i,j+1,grid);
+        solve(i-1,j+1,grid);
+        solve(i+1,j-1,grid);
+        solve(i+1,j+1,grid);
+        solve(i-1,j-1,grid);
+    }
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        n=grid.size();
+        m=grid[0].size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]!='0'){
+                    count++;
+                    solve(i,j,grid);
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
