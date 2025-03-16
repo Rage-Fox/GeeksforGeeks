e783236d237e43638d9cb73a174ddc67887a182a
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-03-16 09:16:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -4,28 +4,36 @@ using namespace std;
 
 
 // } Driver Code Ends
-// Function to return minimum number of jumps to end of array
 
 class Solution {
   public:
-    int minJumps(vector<int>& nums) {
-        // Your code here
-        int l=0,r=0,res=0;
-        while(r<nums.size()-1){
-            int farthest=0;
-            for(int i=l;i<=r;i++){
-                farthest=max(farthest,i+nums[i]);
-            }
-            if(farthest<=l or farthest <=r){
-                return -1;
-            }
-            l=r+1;
-            r=farthest;
-            res++;
+    int minJumps(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int i =0;
+        if(n==1){
+            return 0;
         }
-        return res;
+        if(arr[i] == 0){
+            return -1;
+        }
+        int jumps = 1;          
+        int maxReach = arr[0];  
+        int steps = arr[0];  
+         for (int i = 1; i < n; i++) {
+            if (i == n - 1) return jumps; // Reached the last index
+            maxReach = max(maxReach, i + arr[i]); // Update max reachable index
+            steps--; // Use a step
+            if (steps == 0) { // If no steps left, we must jump
+                jumps++;
+                if (i >= maxReach) return -1; // If stuck, return -1
+                steps = maxReach - i; // Reset steps to the new maxReach
+            }
+        }
+        return -1;
     }
 };
+
 
 
 //{ Driver Code Starts.
@@ -46,7 +54,7 @@ int main() {
             arr.push_back(number);
         }
         Solution obj;
-        cout << obj.minJumps(arr) << endl;
+        cout << obj.minJumps(arr) << endl << "~\n";
     }
     return 0;
 }
