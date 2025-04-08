ed4f10321cb93539cb9a2167c7f8c03209dedc5a
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-04-08 06:33:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bridge edge in a graph/bridge-edge-in-a-graph.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void dfs(int node,vector<int> adj[],int c,int d,vector <int> &vis)
+    {
+        for(auto it: adj[node])
+        {
+            if(!vis[it] && !(node==c && it==d))
+            {
+                vis[it] = 1;
+                dfs(it,adj,c,d,vis);
+            }
+        }
+    }
+    bool isBridge(int V, vector<vector<int>> &edges, int c, int d) {
+        // Code here
+        vector<int> adj[V];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector <int> vis(V,0);
+        
+        dfs(c,adj,c,d,vis);
+        
+        if(!vis[d]) return true;
+        
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        vector<vector<int>> edges(E);
+        int i = 0;
+        while (i++ < E) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            edges[i - 1].push_back(u);
+            edges[i - 1].push_back(v);
+        }
+
+        int c, d;
+        cin >> c >> d;
+
+        Solution obj;
+        bool l = obj.isBridge(V, edges, c, d);
+        if (l)
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
