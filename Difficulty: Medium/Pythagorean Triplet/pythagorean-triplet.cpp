f3bf3fa0b6aa1f6a3b1442fd801b8e3f015b596c
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-05-25 09:11:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+        set<int> sets;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] <= 0) continue;
+            sets.insert(arr[i] * arr[i]);
+        }
+        for(int a : sets) {
+            for(int b : sets) {
+                if(sets.find(a + b) != sets.end()) return true;
+            }
+        }
+        return false;
+    }
+};
