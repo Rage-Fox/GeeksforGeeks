0b30848897117a1f9224848ebe19b2a6fe39ac38
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-06-24 06:47:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Lexicographically Largest String After Deleting K Characters/lexicographically-largest-string-after-deleting-k-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string maxSubseq(string& s, int k) {
+        // code here
+        string st;
+        for (char c : s) {
+            while (k && st.size() && st.back() < c) {
+                st.pop_back();
+                k--;
+            }
+            st.push_back(c);
+        }
+        while (k-- && st.size()) st.pop_back();
+        return st.empty() ? "" : st;
+    }
+};
