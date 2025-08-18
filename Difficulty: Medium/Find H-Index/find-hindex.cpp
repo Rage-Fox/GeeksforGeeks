77b95ad01b50d591150cfcd558204c761a0b54d5
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-08-18 19:23:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,57 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find hIndex
     int hIndex(vector<int>& citations) {
         // code here
-        sort(citations.rbegin(),citations.rend());
-        int ind=0;
-        for(int i=0;i<citations.size();i++){
-            if(citations[i]>=i+1){
-                ind=i+1;
-            }
-            else{
-                break; 
-            }
+        int n = citations.size();
+        int l = 0, r = n + 1;
+        while(r > l + 1) {
+            int m = (l + r) / 2, cnt = 0;
+            for(int i = 0; i < n; i++) cnt += (citations[i] >= m);
+            if(cnt >= m) l = m;
+            else r = m;
         }
-        return ind;
+        return l;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int test_cases;
-    cin >> test_cases;
-    cin.ignore();
-    while (test_cases--) {
-        string input;
-        vector<int> arr;
-
-        // Read the array from input line
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Solution instance to invoke the function
-        Solution ob;
-        int result = ob.hIndex(arr);
-
-        cout << result << endl;
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
