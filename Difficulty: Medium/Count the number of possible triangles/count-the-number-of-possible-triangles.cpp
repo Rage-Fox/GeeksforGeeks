2131db15fc890078f994588c3900154828c9ce37
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-08-27 06:33:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,58 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to count the number of possible triangles.
     int countTriangles(vector<int>& arr) {
         // code here
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
         int ans=0;
-        for(int c=n-1;c>=2;c--){
-            int a=0;
-            int b=c-1;
-            while(a<b){
-                if(arr[a]+arr[b]>arr[c]){
-                    ans+=b-a;
-                    b--;
-                }
-                else{
-                    a++;
+        for(int i=n-1;i>1;i--){
+            int l=0, r=i-1;
+            while(r>l){
+                if((arr[l]+arr[r]) > arr[i]){
+                    ans += (r-l);
+                    r--;
+                }else{
+                    l++;
                 }
             }
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
