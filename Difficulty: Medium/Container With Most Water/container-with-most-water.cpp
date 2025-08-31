10927fdc559e5fa9492f73a88986cf7de4b3ff57
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-08-31 09:32:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -1,55 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-
   public:
     int maxWater(vector<int> &arr) {
         // code here
-        int len=arr.size();
-        int left=0,right=len-1;
-        int ans =0;
-        while(left<right){
-            int tempwater=min(arr[left],arr[right])*(right-left);
-            ans=max(ans,tempwater);
-            if(arr[left]<arr[right]){
-                left++;
-            }
-            else{
-                right--;
-            }
+        int n = arr.size();
+        vector<vector<int>> tmp(n, vector<int>(2, 0));
+        for(int i = 0; i < n; i++) {
+            tmp[i][0] = arr[i]; tmp[i][1] = i;
+        }
+        sort(tmp.begin(), tmp.end());
+        int mini = n, maxi = -1, ans = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(tmp[i][1] - mini > 0) ans = max(ans, tmp[i][0] * (tmp[i][1] - mini));
+            if(maxi - tmp[i][1] > 0) ans = max(ans, tmp[i][0] * (maxi - tmp[i][1]));
+            mini = min(mini, tmp[i][1]);
+            maxi = max(maxi, tmp[i][1]);
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
