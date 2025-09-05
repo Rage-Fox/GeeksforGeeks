4dd5dde4246aac070662e276674fe10ff5905bd1
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-09-05 06:47:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -1,9 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Link list Node */
-class Node {
+/* Node is defined as
+  class Node {
   public:
     int data;
     Node* next;
@@ -13,118 +9,35 @@ class Node {
         next = nullptr;
     }
 };
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-void freeList(struct Node* head) {
-    struct Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        delete temp;
-    }
-}
-
-
-// } Driver Code Ends
-
-/*
-
-  Node is defined as
-  struct Node {
-    int data;
-    struct Node *next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
 */
 class Solution {
   public:
-    // Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node* head) {
         // code here
-        int zero = 0;
-        int one = 0;
-        int two = 0;
-        Node* temp = head;
-        while(temp != NULL){
-            int ele = temp->data;
-            if(ele == 0)
-                zero++;
-            else if(ele == 1)
-                one++;
-            else
-                two++;
-            temp = temp->next;
-        } 
-        temp = head;
-        while(zero != 0){
-            temp->data = 0;
-            temp = temp->next;
-            zero--;
+        Node* curr=head;
+        int n=0;
+        int zeroCnt = 0;
+        int oneCnt = 0;
+        int twoCnt = 0;
+        while(curr!=NULL){
+            if(curr->data ==0) zeroCnt+=1;
+            else if(curr->data==1) oneCnt+=1;
+            else twoCnt +=1;
+            curr=curr->next;
         }
-        while(one != 0){
-            temp->data = 1;
-            temp = temp->next;
-            one--;
+        curr=head;
+        while (zeroCnt--) {
+            curr->data = 0;
+            curr = curr->next;
         }
-        while(two != 0){
-            temp->data = 2;
-            temp = temp->next;
-            two--;
-        }  
+        while (oneCnt--) {
+            curr->data = 1;
+            curr = curr->next;
+        }
+        while (twoCnt--) {
+            curr->data = 2;
+            curr = curr->next;
+        }
         return head;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-// Driver program to test above functions
-int main() {
-    int t, k;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-
-    while (t--) {
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-
-        stringstream ss(input);
-        Node *head = nullptr, *tail = nullptr;
-        int x;
-
-        if (ss >> x) {
-            head = new Node(x);
-            tail = head;
-
-            while (ss >> x) {
-                tail->next = new Node(x);
-                tail = tail->next;
-            }
-
-            // Link the last node to the head if k is 1 to make it circular
-            if (k == 1) {
-                tail->next = head;
-            }
-        }
-
-        Solution ob;
-        Node* newHead = ob.segregate(head);
-        printList(newHead);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
