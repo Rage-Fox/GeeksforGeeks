76729def463fd3e09e39eb10675c35ac6b830d73
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-09-17 06:56:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.cpp
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -1,75 +1,43 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
     string decodedString(string &s) {
         // code here
-        int n = s.length();
-        stack<string>st;
-        string str="";
-        int i=0;
-        while(i<n){
-            if(s[i]==']'){
-                string str="";
-                int cnt=0;
-                while(!st.empty() && st.top()>="a" && st.top()<="z"){
-                    string ch = st.top();
-                    st.pop();
-                    str = ch + str;
-                }
-                if(!st.empty() && st.top()=="[") st.pop();
-                int k=1;
-                while(!st.empty() && st.top()>="0" && st.top()<="9"){
-                    string ch = st.top();
-                    int c = stoi(ch);
-                    st.pop();
-                    cnt = k*(c) + cnt;
-                    k = k*10;
-                }
-                string final = "";
-                for(int i=0;i<cnt;i++) final+=str;
-                if(final.length()) st.push(final);
-            }
-            else{
-                string str(1,s[i]);
-                st.push(str);
-            }
-            i++;
+        stack<string> st;
+        int i = s.size()-1;
+        while(i>=0){
+          if(s[i]=='['){
+              string cur = "";
+              while(st.top()!="]"){
+                  cur += st.top();
+                  st.pop();
+              }
+              st.pop();
+              string mkp = "";
+              while((!st.empty()) && (st.top()!="]")){
+                  mkp += st.top();
+                  st.pop();
+              }
+              i--;
+              string p;
+              while(i>=0 && (s[i]>='0' && s[i]<='9')){
+                  p.push_back(s[i]);
+                  i--;
+              }
+              reverse(p.begin(), p.end());
+              int t = stoi(p);
+              string newTemp = "";
+              for(int k=0;k<t;k++){
+                  newTemp += cur;
+              }
+              st.push(newTemp + mkp);
+          }
+          else{
+              string p = "";
+              p += s[i];
+              st.push(p);
+              i--;
+          }
         }
-        string ans = "";
-        while(!st.empty()){
-            string str =st.top();
-            ans = str+ans;
-            st.pop();
-        }
-        return ans;
+        return st.top();
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-
-        Solution ob;
-        cout << ob.decodedString(s) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
