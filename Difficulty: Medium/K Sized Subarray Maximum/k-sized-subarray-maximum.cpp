b16b44c8b813dd7e38632178d4247ba902ceb3f0
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-01-12 06:48:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,71 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
         // code here
-        deque<int>dq;
-        vector<int>ans;
-        int i=0,j=0,n=arr.size();
+        map<int,int,greater<int>>mp;
+        int i = 0,j = 0,n = arr.size();
+        vector<int> ans;
         while(j<n){
-            while(dq.size()>0 and arr[dq.front()]<arr[j]){
-                dq.pop_front();
-            }
-            dq.push_front(j);
-            if(j-i+1<k){
-                j++;
-            }
-            else{
-                ans.push_back(arr[dq.back()]);
-                if(i==dq.back()){
-                    dq.pop_back();
+            mp[arr[j]]++;
+            if(j-i+1 ==k){
+                ans.push_back(mp.begin()->first);
+                mp[arr[i]]--;
+                if(mp[arr[i]] == 0){
+                    mp.erase(arr[i]);
                 }
                 i++;
-                j++;
             }
+            j++;
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution obj;
-        vector<int> res = obj.maxOfSubarrays(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
