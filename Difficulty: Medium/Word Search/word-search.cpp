3f7df7a9db35907884a69132d9ba0729a13d4f63
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-01-27 06:27:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.cpp
+++ Difficulty: Medium/Word Search/word-search.cpp
@@ -1,67 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-public:
-    int move[5] = {0, 1, 0, -1, 0};
-    bool dfs(int i, int j, int idx, vector<vector<char>>& board, string &word, int &m, int &n) {
-        if(idx == word.size()) return 1;
-        char ch = board[i][j];
-        board[i][j] = '0';
-        bool res = 0;
-        for(int k = 0; k < 4; k++) {
-            int newI = i + move[k];
-            int newJ = j + move[k + 1];
-            if(newI >= 0 && newI < m && newJ >= 0 && newJ < n && 
-               board[newI][newJ] != '0' && board[newI][newJ] == word[idx]) {
-                res |= dfs(newI, newJ, idx + 1, board, word, m, n);
+  public:
+    vector<int> delRow = {0, -1, 0, 1};
+    vector<int> delCol = {-1, 0, 1, 0};
+    bool dfs(int ind, int row, int col, vector<vector<char>>& mat, int n, int m,
+             string& word, vector<vector<int>>& vis) {
+        if (ind == word.size())
+            return true;
+        for (int i = 0; i < 4; i++) {
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                mat[nRow][nCol] == word[ind] && vis[nRow][nCol] == -1) {
+                vis[nRow][nCol] = 1;
+                if (dfs(ind + 1, nRow, nCol, mat, n, m, word, vis))
+                    return true;
+                vis[nRow][nCol] = -1;
             }
         }
-        board[i][j] = ch;
-        return res;
+        return false;
     }
-    bool isWordExist(vector<vector<char>>& board, string word) {
+    bool isWordExist(vector<vector<char>> &mat, string &word) {
         // Code here
-        int m = board.size();
-        int n = board[0].size();
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(board[i][j] == word[0]) {
-                    if(dfs(i, j, 1, board, word, m, n)) return 1;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == word[0]) {
+                    vis[i][j] = 1;
+                    if (dfs(1, i, j, mat, n, m, word, vis))
+                        return true;
+                    vis[i][j] = -1;
                 }
             }
         }
-        return 0;
+        return false;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<char>> mat(n, vector<char>(m, '*'));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                cin >> mat[i][j];
-        string word;
-        cin >> word;
-        Solution obj;
-        bool ans = obj.isWordExist(mat, word);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
