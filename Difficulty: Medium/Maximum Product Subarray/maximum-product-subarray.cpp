489fca312f7c5e11b84b6288260ae9b4d50ceaa3
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-02-08 07:39:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -1,64 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find maximum product subarray
     int maxProduct(vector<int> &arr) {
-        // Your Code Here
-        int leftToRight = 1;
-        int rightToLeft = 1;
+        // code here
         int n = arr.size();
-        int result = arr[0];
-        for (int i = 0; i < n; i++) {
-            if (leftToRight == 0)
-                leftToRight = 1;
-            if (rightToLeft == 0)
-                rightToLeft = 1;
-            leftToRight *= arr[i];
-            rightToLeft *= arr[n-i-1];
-            result = max({result, leftToRight, rightToLeft});
+        int ma = arr[0];
+        int mi = ma;
+        int ans = ma;
+        for(int i=1;i<n;i++){
+            if(arr[i]<0)
+                swap(ma,mi);
+            ma = max(arr[i],arr[i]*ma);
+            mi = min(arr[i],arr[i]*mi);
+            ans = max(ans,ma);
         }
-        return result;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        // int n, i;
-        // cin >> n;
-        // vector<int> arr(n);
-        // for (i = 0; i < n; i++) {
-        //     cin >> arr[i];
-        // }
-
-        vector<int> arr;
-        string input;
-
-        // Read array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        auto ans = ob.maxProduct(arr);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
