91e1fa2760255e42c3bdb3b03563d60878e84738
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-03-17 06:33:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -1,89 +1,6 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 /*
-struct Node {
+class Node {
+  public:
     int data;
     Node *left;
     Node *right;
@@ -94,82 +11,56 @@ struct Node {
     }
 };
 */
+
 class Solution {
   public:
-    Node* findNode(Node*&root,int target,unordered_map<Node*,Node*>&par,Node*&ptr){
-        if(!root){
-            return nullptr;
+    void inorder(Node*root,unordered_map<Node*,vector<Node*>>&omap,Node*parent) {
+        if(root==NULL) {
+            return;
         }
-        par[root] = ptr;
-        if(root->data == target){
-            return root;
+        inorder(root->left,omap,root);
+        if(root->left) {
+            omap[root].push_back(root->left);
         }
-        Node*t1 = findNode(root->left,target,par,root);
-        Node*t2 = findNode(root->right,target,par,root);
-        if(t1){
-            return t1;
+        if(root->right) {
+            omap[root].push_back(root->right);
         }
-        return t2;
+        if(parent) {
+            omap[root].push_back(parent);
+        }
+        inorder(root->right,omap,root);
     }
-    int minTime(Node* root, int target){
-        // Your code goes here
-        unordered_map<Node*,Node*>par;
-        Node*ptr = nullptr;
-        Node* targetNode = findNode(root,target,par,ptr);
-        int ans = 0;
+    int minTime(Node* root, int target) {
+        // code here
+        unordered_map<Node*,vector<Node*>>omap;
+        inorder(root,omap,NULL);
+        unordered_map<Node*,bool>visited;
+        Node* finders=NULL;
+        for(auto i:omap) {
+            if(i.first->data==target) {
+                finders=i.first;
+            }
+            visited[i.first]=false;
+        }
         queue<Node*>q;
-        unordered_map<Node*,int>vis;
-        vis[targetNode]++;
-        q.push(targetNode);
-        while(!q.empty()){
-            int siz = q.size();
-            for(int i=0;i<siz;i++){
-                Node*curr = q.front();
+        q.push(finders);
+        visited[finders]=true;
+        int count=0;
+        while(!q.empty()) {
+            count++;
+            int counter=q.size();
+            for(int i=0;i<counter;i++) {
+                Node* vertex=q.front();
                 q.pop();
-                if(curr->left and vis.find(curr->left)==vis.end()){
-                    vis[curr->left]++;
-                    q.push(curr->left);
-                }
-                if(curr->right and vis.find(curr->right)==vis.end()){
-                    vis[curr->right]++;
-                    q.push(curr->right);
-                }
-                if(par[curr] and vis.find(par[curr])==vis.end()){
-                    vis[par[curr]]++;
-                    q.push(par[curr]);
+                vector<Node*>sides=omap[vertex];
+                for(int j=0;j<sides.size();j++) {
+                    if(!visited[sides[j]]) {
+                        visited[sides[j]]=true;
+                        q.push(sides[j]);
+                    }
                 }
             }
-            ans++;
         }
-        return ans-1;
+        return count-1;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() 
-{
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) 
-    {    
-        string treeString;
-        getline(cin, treeString);
-        // cout<<treeString<<"\n";
-        int target;
-        cin>>target;
-        // cout<<target<<"\n";
-
-        Node *root = buildTree(treeString);
-        Solution obj;
-        cout<<obj.minTime(root, target)<<"\n"; 
-
-        cin.ignore();
-
-    }
-
-
-    return 0;
-}
-
-// } Driver Code Ends
