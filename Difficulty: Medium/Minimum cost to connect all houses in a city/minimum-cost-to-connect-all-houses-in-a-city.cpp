2a445bc7e3dee7dd7173da26a706fb1e00973a22
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-03-30 07:00:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
@@ -1,82 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int minCost(vector<vector<int>>& houses) {
         // code here
-        // Prim's Algorithm
-        int V = houses.size();
-        vector<vector<pair<int,int>>> adj(V);
-        for(int i=0; i<V; i++){
-            for(int j=i+1; j<V; j++){
-                int x1 = houses[i][0];
-                int y1 = houses[i][1];
-                int x2 = houses[j][0];
-                int y2 = houses[j][1];
-                int d = abs(x1-x2) + abs(y1-y2);
-                adj[i].push_back({j,d});
-                adj[j].push_back({i,d});
-            }
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        vector<bool> inMST(V,false);
-        int sum = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        long long ans=0LL;
+        auto manDis = [&](vector<int> &a, vector<int> &b){
+            return abs(a[0]-b[0]) + abs(a[1]-b[1]);  
+        };
+        int n=houses.size();
+        vector<bool> visited(n, false);
         while(!pq.empty()){
-            auto p = pq.top();
+            auto [dis, i]=pq.top();
             pq.pop();
-            int wt = p.first;
-            int u = p.second;
-            if(inMST[u] == true){
+            if(visited[i])
                 continue;
-            }
-            inMST[u] = true;
-            sum += wt;
-            for(auto& node: adj[u]){
-                int nbr = node.first;
-                int w = node.second;
-                if(inMST[nbr] == false){
-                    pq.push({w,nbr});
+            visited[i]=true;
+            ans += (1LL*dis);
+            for(int j=0; j<n; j++){
+                if(!visited[j]){
+                    int d=manDis(houses[i], houses[j]);
+                    pq.push({d, j});
                 }
             }
         }
-        return sum;
+        return (int)ans;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> edges;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 2; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        Solution obj;
-
-        cout << obj.minCost(edges);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
