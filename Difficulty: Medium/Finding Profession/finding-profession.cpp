a2fbc2a8864564f7d6bc04b044951454fdb267b6
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-07 07:27:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Finding Profession/finding-profession.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    string profession(int level, int pos) {
+        // code here
+        if(__builtin_popcount(pos-1)&1){
+            return "Doctor";
+        }
+        return "Engineer";
+    }
+};
