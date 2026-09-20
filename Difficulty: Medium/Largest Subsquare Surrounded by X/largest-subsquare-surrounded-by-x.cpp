ff67416c5d1faeb561cf58a2ad2b07eb2a71e010
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-09-20 07:02:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest Subsquare Surrounded by X/largest-subsquare-surrounded-by-x.cpp
@@ -0,0 +1,54 @@
+class Solution {
+	public:
+	int largestSubsquare(vector<vector<char>> &mat) {
+		// code here
+		int n=mat.size();
+		vector<vector<pair<int, int>>> v(n, vector<pair<int, int>> (n, {-1, -1}));
+		for (int row = 0; row<n; row++) {
+			int bul = -1;
+			for (int j = n - 1; j >= 0; j--) {
+				if (mat[row][j] == 'X') {
+					if (bul == -1)
+						bul = j;
+				}
+				else {
+					bul = -1;
+				}
+				v[row][j].first = bul;
+			}
+		}
+		for (int col = 0; col<n; col++) {
+			int bul = -1;
+			for (int j = n - 1; j >= 0; j--) {
+				if (mat[j][col] == 'X') {
+					if (bul == -1)
+						bul = j;
+				}
+				else {
+					bul = -1;
+				}
+				v[j][col].second = bul;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i<n; i++) {
+			for (int j = 0; j<n; j++) {
+				int rightMostIdx = v[i][j].first;
+				int bottomMostIdx = v[i][j].second;
+				int rightReach = rightMostIdx - j + 1;
+				int bottomReach = bottomMostIdx - i + 1;
+				int currSz = min(rightReach, bottomReach);
+				while (currSz > ans) {
+					int currRightsBottomReach = v[i][j + currSz - 1].second - i + 1;
+					int currBottomsRightReach = v[i + currSz - 1][j].first - j + 1;
+					if (currRightsBottomReach >= currSz && currBottomsRightReach >= currSz) {
+						ans = currSz;
+						break;
+					}
+					currSz--;
+				}
+			}
+		}
+		return ans;
+	}
+};
